1eb231ada84c2342273964ea506ce1cb29a8f7f8
Bayrem Gharsellaoui <bayrem.gharsellaoui@gmail.com>
2026-05-22 14:03:10 +0100

    Adding more unit tests

--- tests/drivers/pn532/src/main.c
+++ tests/drivers/pn532/src/main.c
@@ -28,3 +28,61 @@ ZTEST(pn532, test_get_firmware_version)
     zassert_equal(0, version.ver);
     zassert_equal(0, version.rev);
 }
+
+/**
+ * @brief Test inlist passive target for the PN532 driver
+ */
+ZTEST(pn532, test_in_list_passive_target)
+{
+	const struct device *dev = DEVICE_DT_GET_ONE(nxp_pn532);
+
+    zassert_equal(-EINVAL, pn532_in_list_passive_target(NULL));
+}
+
+/**
+ * @brief Test in data exchange for the PN532 driver
+ */
+ZTEST(pn532, test_in_data_exchange)
+{
+	const struct device *dev = DEVICE_DT_GET_ONE(nxp_pn532);
+    uint8_t send[] = {0xDE, 0xAD, 0xBE, 0xEF};
+    uint8_t send_length = sizeof(send);
+    uint8_t response[32] = {0};
+
+    zassert_equal(-EINVAL, pn532_in_data_exchange(NULL, NULL, 0, NULL, NULL));
+
+    zassert_equal(-EINVAL, pn532_in_data_exchange(dev, NULL, 0, NULL, NULL));
+
+    zassert_equal(-EINVAL, pn532_in_data_exchange(dev, send, NULL, NULL, NULL));
+
+    zassert_equal(-EINVAL, pn532_in_data_exchange(dev, send, send_length, NULL, NULL));
+
+    zassert_equal(-EINVAL, pn532_in_data_exchange(dev, send, send_length, response, NULL));
+}
+
+/**
+ * @brief Test set serial baudrate for the PN532 driver
+ */
+ZTEST(pn532, test_set_serial_baudrate)
+{
+	const struct device *dev = DEVICE_DT_GET_ONE(nxp_pn532);
+
+    zassert_equal(-EINVAL, pn532_set_serial_baudrate(NULL, 0));
+    zassert_equal(-EINVAL, pn532_set_serial_baudrate(dev, 0));
+}
+
+/**
+ * @brief Test GPIO write to the PN532 driver
+ */
+ZTEST(pn532, test_gpio_pin_set)
+{
+    zassert_equal(-EINVAL, pn532_gpio_pin_set(NULL, 0));
+}
+
+/**
+ * @brief Test GPIO read from the PN532 driver
+ */
+ZTEST(pn532, test_gpio_pin_get)
+{
+    zassert_equal(-EINVAL, pn532_gpio_pin_get(NULL));
+}
